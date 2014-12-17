330c53aaca3c8fef70fe5bd51a23fd17bf680384
Rindra R <rindranirina@gmail.com>
2014-12-17 10:36:06 -0800

    added predict

--- nnet.jl
+++ nnet.jl
@@ -48,8 +48,6 @@ end
 	N = size(X,1)
 	d = [nn.numInputs, reshape(nn.numHidden, length(nn.numHidden),1), nn.numOutputs]
 
-	μ = 0.1
-
 	# Initialize weights 
 	initWeights(net, X)
 
@@ -61,11 +59,6 @@ end
   		if n % 10 == 0 println("Sample: $n") end
   		# Local variables within scope of each iteration
   		xn, yn = X[n,:]', y[n]
-
-
-
-  		# yn is encoded as one-of-C vector
-  		yv = mask(ones(C), yn)
  		
   		# Compute output vectors x(l), l = 1,...,L+1
   		x, s = forwardPropagate(nn, xn)
@@ -76,53 +69,68 @@ end
   		# Compute sensitivity matrices Δ[l], l = 1,...,L	 
   		local Δ = backPropagate(nn, x)
 
-  		# updateWeights(nn, x, y yv, h)
-			# Gradient of error w.r.t softmax output vector
-			∇e_h = - vec(yv ./ h)
-
-  		# Gradient of error w.r.t weight matrix W[L] 
-  		# assuming softmax output activation and cross-entropy error
-  		# 
-  		∇e_WL = zeros(Float64, d[L]+1,C)
-  		for i in 1:d[L]+1
-  		  for j in 1:C 
-  				∇e_WL[i,j] = x[L][i] * (h[j] - yv[j]) 
-  			end
-  		end
-
-  		# SGD update of weight matrix W[L]
-  		nn.weights[L] = nn.weights[L] - μ * ∇e_WL
-
-  		# Gradient of error w.r.t weight matrix @ input W[1] 
-  		∇e_W1 = zeros(Float64, d[1],d[2])
-  		for i in 1:d[1]
-
-  		  for j in 1:C 
-  				∇e_W1[i,j] = x[1][i] * dot(∇e_h, vec(Δ[1][:,j]))
-  			end
-  		end
-
-  		# SGD update of weight matrix W[1]
-  		nn.weights[1] = nn.weights[1] - μ * ∇e_W1
-
-  		# Gradient of error w.r.t weight matrices of hidden layers
-  		for l = 2:L-1  # changed from 1:L-1
-  			# Gradient of error w.r.t weight matrix Wl
-  			∇e_Wl = zeros(Float64, d[l]+1, d[l+1])
-  				for i in 1:d[l]+1
-  					for j in 1:d[l+1] 
-  						∇e_Wl[i,j] = x[l][i] * dot(∇e_h, vec(Δ[l][:,j]))
-  					end
-					end
-
-  			# SGD update the weight matrices 
-  			nn.weights[l] = nn.weights[l] - μ * ∇e_Wl
-  		end
+  		updateWeights(nn, Δ, x, yn, h)
+			
   	end   	
 	end
 
   # return in-sample error (optionally)
 end
+
+function updateWeights(nn::NeuralNet, Δ, x, yn, h)
+
+	L = nn.numLayers
+	C = nn.numOutputs
+	d = [nn.numInputs, reshape(nn.numHidden, length(nn.numHidden),1), nn.numOutputs]
+
+	μ = 0.1
+
+	# yn is encoded as one-of-C vector
+	yv = mask(ones(C), yn)
+
+	# Gradient of error w.r.t softmax output vector
+	∇e_h = - vec(yv ./ h)
+
+	# Gradient of error w.r.t weight matrix W[L] 
+	# assuming softmax output activation and cross-entropy error
+	# 
+	∇e_WL = zeros(Float64, d[L]+1,C)
+	for i in 1:d[L]+1
+	  for j in 1:C 
+			∇e_WL[i,j] = x[L][i] * (h[j] - yv[j]) 
+		end
+	end
+
+	# SGD update of weight matrix W[L]
+	nn.weights[L] = nn.weights[L] - μ * ∇e_WL
+
+	# Gradient of error w.r.t weight matrix @ input W[1] 
+	∇e_W1 = zeros(Float64, d[1],d[2])
+	for i in 1:d[1]
+
+	  for j in 1:C 
+			∇e_W1[i,j] = x[1][i] * dot(∇e_h, vec(Δ[1][:,j]))
+		end
+	end
+
+	# SGD update of weight matrix W[1]
+	nn.weights[1] = nn.weights[1] - μ * ∇e_W1
+
+	# Gradient of error w.r.t weight matrices of hidden layers
+	for l = 2:L-1  # changed from 1:L-1
+		# Gradient of error w.r.t weight matrix Wl
+		∇e_Wl = zeros(Float64, d[l]+1, d[l+1])
+			for i in 1:d[l]+1
+				for j in 1:d[l+1] 
+					∇e_Wl[i,j] = x[l][i] * dot(∇e_h, vec(Δ[l][:,j]))
+				end
+			end
+
+		# SGD update the weight matrices 
+		nn.weights[l] = nn.weights[l] - μ * ∇e_Wl
+	end
+	nn.weights
+end	
 
 ## Initialization of weights
 function initWeights (nn::NeuralNet, X::Array{Float64})
@@ -184,9 +192,9 @@ end
   s[L] = (nn.weights[L])' * x[L]
   
   x[L+1] = if C == 1 
-             map(logit,s[L])
+             logit(s[L])
            else 
-           	 map(softmax,s[L])
+           	 softmax(s[L])
            end
 
 	# return output and input vectors
@@ -241,11 +249,12 @@ end
 
 
 ## Prediction functions 
-function predict(net::NeuralNet, testData)
-
-	# return class labels 
+# return the maximum a posteriori probability and label of predicted class 
+@debug function predict(net::NeuralNet, xnew)
+	x,s = forwardPropagate(net, xnew)
+	
+	prob, class = findmax(x[end])
 end
-
 
 ##---- Test
 net = NeuralNet(2, 2, 3, [5])
@@ -267,3 +276,5 @@ X = randn(50,20); y = rand(1:3,50)
 options = FitOptions()
 fit(net, X, y, options::FitOptions, 100)
 println(net.weights)
+xtest = randn(10,20)
+yhat = map((x) -> predict(net, x), map(i -> vec(xtest[i,:]), 1:size(xtest,1)))
