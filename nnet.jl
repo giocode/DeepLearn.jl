a6134b7c514659bbc97fa4048dcab2dd008f4563
Rindra R <rindranirina@gmail.com>
2014-12-18 04:03:48 -0800

    added data

--- nnet.jl
+++ nnet.jl
@@ -4,16 +4,18 @@ include("helpers.jl")
 
 using Debug
 
+##----------------------------------------------------------------------
 ## Neural Network structure 
 type NeuralNet
-	numLayers::Int   						
-	numInputs::Int
-	numOutputs::Int 
-	numHidden::Array{Int} 
-	activFunc::String 
-	weights
+	numLayers::Int   					# Number of layers (excluding input)		
+	numInputs::Int 						# Input dimension (no bias node)
+	numOutputs::Int 					# Output dimension
+	numHidden::Array{Int} 		# Number of units at each hidden layer
+	activFunc::String 				# Activation function of hidden units
+	weights 									# Weight matrices 
 
 	NeuralNet(L,inputs, outputs,units,activ, ws) = 
+		# assert dimensions agree
 	  if length(units) != L-1 
 	  	error("numHidden must be a vector of length $(L-1)")
 	  elseif ∉(activ, ["tanh", "logit"])
@@ -27,18 +29,27 @@ end
 NeuralNet(L, inputs, outputs) = NeuralNet(L, inputs, outputs, 3 * ones(L-1,1), "tanh", cell(L,1))
 NeuralNet(L, inputs, outputs, units) = NeuralNet(L, inputs, outputs, units, "tanh", cell(L,1))
 
-
+##----------------------------------------------------------------------
 ## Function for pre-processing training data 
 ##	Input: training data
 ##	Ouput: pair of feature matrix and class labels 
-##  		- x is a n x d feature matrix
+##  		- X is a n x d feature matrix
 ##			- y is an n-element vector with values from 1 to numOutputs
-function extractSample(trainData)
+##----------------------------------------------------------------------
+function extractSample(Xtrain, Ytrain, n)
 	X, y
 end
 
-## Fitting functions 
-@debug function fit(nn::NeuralNet, X, y, options::FitOptions, maxIter::Int)
+##----------------------------------------------------------------------
+## Fit: train the neural network model 
+## Inputs:
+##		- Neural network structure
+##		- Matrix of features X with dimension N x p
+##		- Class label vector of dimension N x 1 (yn ∈ {1,...,C})
+##		- fitting options 
+##		- maximum number of optimization iterations 
+##----------------------------------------------------------------------
+@debug function fit!(nn::NeuralNet, X, y, options::FitOptions, maxIter::Int)
 
 	# assume trainData if preprocessed to make learning faster
 	# e.g. successive examples come from different classes 
@@ -48,39 +59,157 @@ end
 	d = [nn.numInputs, reshape(nn.numHidden, length(nn.numHidden),1), nn.numOutputs]
 
 	# Initialize weights 
-	initWeights(net, X)
-
+	initWeights!(nn, X)
 
 	# Iteration of whole batch training
 	for i = 1:maxIter
-		if i % 10 == 0 println("Iteration: $i") end
+		if i % 10 == 0 tic; println("Pass data Iteration: $i") end
   	for n = 1:N 
-  		if n % 10 == 0 println("Sample: $n") end
+  		if n % 50 == 0 println("Samples: $n") end
   		# Local variables within scope of each iteration
   		xn, yn = X[n,:]', y[n]
  		
   		# Compute output vectors x(l), l = 1,...,L+1
   		x, s = forwardPropagate(nn, xn)
 
-  		# Softmax output
-  		h = x[L+1]
-
   		# Compute sensitivity matrices Δ[l], l = 1,...,L	 
   		local Δ = backPropagate(nn, x)
 
-  		updateWeights(nn, Δ, x, yn, h)
-			
-  	end   	
+  		# Update the weights
+  		updateWeights!(nn, Δ, x, yn)
+  	end  
+  	if i % 10 == 0 toc end  	
 	end
 
   # return in-sample error (optionally)
 end
 
-function updateWeights(nn::NeuralNet, Δ, x, yn, h)
+##----------------------------------------------------------------------
+## initWeights: initializate weights matrices
+## Inputs:
+##		- Neural network structure
+##		- Sensitivity matrices Δ[l], ∀l ∈ {1,..,L}
+##		- Output vectors x[l], ∀l ∈ {1,..,L+1} 
+##		- Class label of example: yn 
+##----------------------------------------------------------------------
+
+@debug function initWeights!(nn::NeuralNet, X::Array{Float64})
+	L = nn.numLayers
+	N = size(X,1)
+	d = [nn.numInputs, reshape(nn.numHidden, length(nn.numHidden),1), nn.numOutputs]
+	
+	# variance must be smaller than inverse of max_n{squaredNorm(xn)}
+	σ = map(i -> norm(X[i,:]')^2, [1:N]) |> maximum |> sqrt
+
+	# first layer 
+	nn.weights[1] = 1e-1 * (1/σ) * randn(d[1],d[2])
+
+	# subsequent layers
+	for l = 2:L
+		nn.weights[l] = 1e-1 * (1/σ) * randn(d[l]+1,d[l+1])
+	end
+
+	# return the initial weights
+	nn.weights
+end
+
+##----------------------------------------------------------------------
+## Forward propagation subroutine
+## Inputs: 
+## 		- Training example: xn
+## 		- Neural network structure: number of layers, number of units
+## Outputs: 
+##		- L+1 output vectors x[l] (including input vector x[1] = xn)
+##		- L input vectors s[l] 
+##----------------------------------------------------------------------
+@debug function forwardPropagate(nn::NeuralNet, xn::Array{Float64})
+	# println("Forward propagation..")
+	# Network topology
+	L = nn.numLayers   					 # number of all layers excluding input 
+	C = nn.numOutputs
+	
+	# Cells containg L input vectors and L+1 output vectors
+	s = cell(L,1) 	# inputs of all layers 
+	x = cell(L+1,1) # outputs of all layers
+
+	# Compute input s[l] and output x[l+1] at each layer
+	# Input vector x[1] has no bias node
+	x[1] = xn 
+	# Hidden layers
+  for l in 1:L-1
+  	W = (nn.weights[l])
+  	s[l] = W'*x[l] 
+  	x[l+1] = [1, map(tanh, s[l])]   # use tanh
+  end
+  # Output layer
+  s[L] = (nn.weights[L])' * x[L]
+  x[L+1] = if C == 1 
+             logit(s[L])
+           else 
+           	 softmax(s[L])
+           end
+
+	# return output and input vectors
+  x, s
+end
+
+##----------------------------------------------------------------------
+## Backpropagation subroutine
+## Inputs: 
+##		- L+1 output vectors x[l] (including input vector x[1] = xn)
+##		- L input vectors s[l] 
+## Outputs: 
+## 		- Sensitivity matrices Δ[l] for all layer l = 1,...,L
+##----------------------------------------------------------------------
+function backPropagate(nn::NeuralNet, x)
+	# println("Back propagation..")
+	# Network topology
+	L = nn.numLayers   					 # number of all layers excluding input 
+	C = nn.numOutputs
+	d = [nn.numInputs, reshape(nn.numHidden, length(nn.numHidden),1), nn.numOutputs]
+	Δ = cell(L,1) 	
+
+	# Output vector
+	h = x[L+1]	
+
+	# Sensitivy matrix for output layer
+	Δ[L] = [ h[i]*(δ(i,j) - h[j]) for i in 1:C, j in 1:C ]
+
+	# Sensitivy matrix for inner layers
+	for l = L-1:-1:1
+		Δ[l] = zeros(Float64, C, d[l+1]) 
+		W = nn.weights[l+1]
+		for k in 1:C
+			for j in 1:d[l+1]
+				# assuming tanh activation function at hidden units				
+				Δ[l][k,j] = (1 - h[k]^2) * (W[j,:] * Δ[l+1][k,:]')[1]
+			end
+		end
+	end
+
+	# return sensitivity matrices for all layers ∀l = L,...,1
+	Δ
+end
+
+
+##----------------------------------------------------------------------
+## updateWeights: update and return the weight matrices at each iteration
+## Inputs:
+##		- Neural network structure
+##		- Sensitivity matrices Δ[l], ∀l ∈ {1,..,L}
+##		- Output vectors x[l], ∀l ∈ {1,..,L+1} 
+##		- Class label of example: yn 
+## Output: 
+##		- weights matrices
+##----------------------------------------------------------------------
+function updateWeights!(nn::NeuralNet, Δ, x, yn)
 
 	L = nn.numLayers
 	C = nn.numOutputs
 	d = [nn.numInputs, reshape(nn.numHidden, length(nn.numHidden),1), nn.numOutputs]
+
+	# Softmax output
+	h = x[L+1]
 
 	# Step size
 	μ = 0.1
@@ -131,142 +260,21 @@ function updateWeights(nn::NeuralNet, Δ, x, yn, h)
 	nn.weights
 end	
 
-## Initialization of weights
-function initWeights (nn::NeuralNet, X::Array{Float64})
-	L = nn.numLayers
-	N = size(X,1)
-	d = [nn.numInputs, reshape(nn.numHidden, length(nn.numHidden),1), nn.numOutputs]
-	
-	# variance must be smaller than inverse of max_n{squaredNorm(xn)}
-	σ = map(i -> norm(X[i,:]')^2, [1:N]) |> maximum |> sqrt
-
-	# first layer 
-	nn.weights[1] = 1e-3 * (1/σ) * randn(d[1],d[2])
-
-	# subsequent layers
-	for l = 2:L
-		nn.weights[l] = 1e-3 * (1/σ) * randn(d[l]+1,d[l+1])
-	end
-
-	# return the new weights
-	nn.weights
-end
-
 ##----------------------------------------------------------------------
-## Forward propagation subroutine
-## Inputs: 
-## 		- Training example: xn
-## 		- Neural network structure: number of layers, number of units
-## Outputs: 
-##		- L+1 output vectors x[l] (including input vector x[1] = xn)
-##		- L input vectors s[l] 
+## predict: predict the labels of new samples using forward propagation 
+## Inputs:
+##		- Neural network structure
+##		- New samples of size N x p 
+## Output: a vector of pairs containing:
+##		- Maximum posterior probability of predicted class 
+##		- Label of predicted class ∈ {1,...,C}
 ##----------------------------------------------------------------------
-@debug function forwardPropagate(nn::NeuralNet, xn::Array{Float64})
-	# println("Forward propagation..")
-	# Network topology
-	L = nn.numLayers   					 # number of all layers excluding input 
-	C = nn.numOutputs
-
-	# set activation function 
-	# θ = if nn.activFunc == "tanh" 
-	# 			tanh
-	# 	  elseif nn.activFunc == "logit" 
-	# 	  	logit
-	# 	  end
-	
-	# Cells containg L input vectors and L+1 output vectors
-	s = cell(L,1) 	# inputs of all layers 
-	x = cell(L+1,1) # outputs of all layers
-
-	
-	# Compute input s[l] and output x[l+1] at each layer
-	# Input vector x[1] has no bias node
-	x[1] = xn 
-	# Hidden layers
-  for l in 1:L-1
-  	W = (nn.weights[l])
-  	s[l] = W'*x[l] 
-  	x[l+1] = [1, map(tanh, s[l])]   # use tanh
-  end
-  # Output layer
-  s[L] = (nn.weights[L])' * x[L]
-  x[L+1] = if C == 1 
-             logit(s[L])
-           else 
-           	 softmax(s[L])
-           end
-
-	# return output and input vectors
-  x, s
-end
-
-##----------------------------------------------------------------------
-## Backpropagation subroutine
-## Inputs: 
-##		- L+1 output vectors x[l] (including input vector x[1] = xn)
-##		- L input vectors s[l] 
-## Outputs: 
-## 		- Sensitivity matrices Δ[l] for all layer l = 1,...,L
-##----------------------------------------------------------------------
-@debug function backPropagate(nn::NeuralNet, x)
-	# println("Back propagation..")
-	# Network topology
-	L = nn.numLayers   					 # number of all layers excluding input 
-	C = nn.numOutputs
-	d = [nn.numInputs, reshape(nn.numHidden, length(nn.numHidden),1), nn.numOutputs]
-	Δ = cell(L,1) 	
-
-	## Backward propagation
-
-	# Output vector
-	h = x[L+1]	
-
-	# Sensitivy matrix for output layer
-	Δ[L] = [ h[i]*(δ(i,j) - h[j]) for i in 1:C, j in 1:C ]
-
-	# Sensitivy matrix for inner layers
-	for l = L-1:-1:1
-		Δ[l] = zeros(Float64, C, d[l+1]) 
-		W = nn.weights[l+1]
-		for k in 1:C
-			for j in 1:d[l+1]
-				# assuming tanh activation function at hidden units				
-				Δ[l][k,j] = (1 - h[k]^2) * (W[j,:] * Δ[l+1][k,:]')[1]
-			end
-		end
-	end
-
-	# return sensitivity matrices for all layers ∀l = L,...,1
-	Δ
-end
-
-
-## Prediction functions 
-# return the maximum a posteriori probability and label of predicted class 
-@debug function predict(net::NeuralNet, xnew)
+function predict(net::NeuralNet, xnew)
 	x,s = forwardPropagate(net, xnew)
 	prob, class = findmax(x[end])
 end
 
-##---- Test
-net = NeuralNet(2, 2, 3, [5])
-
-# net = NeuralNet(3, 2, 1, [7 3])
-# initWeights(net, randn(20,2))
-# x, s = forwardPropagate(net, randn(net.numInputs,1))
-# backPropagate(net, x)
-
-# # simple
-# net = NeuralNet(2, 2, 3, [5])
-# X = randn(30,2); y = rand(1:3,30)
-
-# not simple
-net = NeuralNet(4, 20, 3, [16 8 6])
-X = randn(50,20); y = rand(1:3,50)
-
-
-options = FitOptions()
-fit(net, X, y, options::FitOptions, 100)
-println(net.weights)
-xtest = randn(10,20)
-yhat = map((x) -> predict(net, x), map(i -> vec(xtest[i,:]), 1:size(xtest,1)))
+# test
+# # not simple
+# net = NeuralNet(4, 20, 3, [16 8 6])
+# X = randn(100,20); y = rand(1:3,100)
